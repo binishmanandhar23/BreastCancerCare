d13d4be4a45c65da23412ff1f2e15d291fd9a109
binishmanandhar23 <binish.manandhar23@gmail.com>
2025-08-06 21:44:59 +0800

    Added Kotlin Firebase Common Database Library

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,12 +1,14 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.ksp)
 
     id("com.google.gms.google-services")
 }
@@ -34,7 +36,10 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(project.dependencies.platform(libs.firebase.android.bom))
             implementation(libs.firebase.config)
+            implementation(libs.firebase.database.ktx)
+            implementation(libs.firebase.common.ktx)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -46,7 +51,19 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.material.icons.extended)
+
+            /*Kotlin Coroutines*/
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+
+
+            /*Firebase Database*/
             implementation("dev.gitlive:firebase-database:2.1.0")
+
+            /*Koin for Dependency Injection*/
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.composeVM)
+            api(libs.koin.annotations)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -83,5 +100,24 @@ android {
 
 dependencies {
     debugImplementation(compose.uiTooling)
+
+    add("kspCommonMainMetadata", libs.koin.ksp.compiler)
+    add("kspAndroid", libs.koin.ksp.compiler)
+    add("kspIosX64", libs.koin.ksp.compiler)
+    add("kspIosArm64", libs.koin.ksp.compiler)
+    add("kspIosSimulatorArm64", libs.koin.ksp.compiler)
 }
 
+// Enables compile-time validation for Koin configuration.
+// Without this, issues are only caught at runtime.
+ksp {
+    arg("KOIN_CONFIG_CHECK","true")
+}
+
+// Ensures the KSP metadata for commonMain is generated before running native compilations.
+// Prevents missing dependency issues by forcing proper task ordering.
+project.tasks.withType(KotlinCompilationTask::class.java).configureEach {
+    if(name != "kspCommonMainKotlinMetadata") {
+        dependsOn("kspCommonMainKotlinMetadata")
+    }
+}

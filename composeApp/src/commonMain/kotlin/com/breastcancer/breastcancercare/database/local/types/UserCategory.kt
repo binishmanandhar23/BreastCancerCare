a5936e085a018eabe3b5b6481005678683b17de7
binishmanandhar23 <binish.manandhar23@gmail.com>
2025-09-10 19:27:09 +0800

    Crash fixes regarding enums

--- composeApp/src/commonMain/kotlin/com/breastcancer/breastcancercare/database/local/types/UserCategory.kt
+++ composeApp/src/commonMain/kotlin/com/breastcancer/breastcancercare/database/local/types/UserCategory.kt
@@ -2,6 +2,10 @@ package com.breastcancer.breastcancercare.database.local.types
 
 enum class UserCategory(val category: String) {
     StartingStrong("starting_strong"),
-    LivingWell("living_well")
+    LivingWell("living_well");
+
+    companion object{
+        fun fromCategory(category: String) = entries.find { it.category == category } ?: StartingStrong
+    }
 }
 
